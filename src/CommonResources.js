e3549e072f5babfd8c17f9498682d5c9c04b2817
rehanuddin1991 <rehan.ictd@gmail.com>
2024-10-25 17:06:12 +0600

    changed

--- src/CommonResources.js
+++ src/CommonResources.js
@@ -1,2 +1,2 @@
-export const backend_uri="http://localhost:5000";
-//export const backend_uri="https://binary-planet.onrender.com";
+//export const backend_uri="http://localhost:5000";
+export const backend_uri="https://binary-planet.onrender.com";
