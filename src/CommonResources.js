b191828ca3de88b81e438d586e875cbd43ae99bc
rehanuddin1991 <rehan.ictd@gmail.com>
2024-10-26 10:31:12 +0600

    link changed

--- src/CommonResources.js
+++ src/CommonResources.js
@@ -1,4 +1,4 @@
 //export const backend_uri="http://localhost:3000";
-export const backend_uri="https://binary-planet-server.vercel.app";
-//export const backend_uri="https://binary-planet.onrender.com";
+//export const backend_uri="https://binary-planet-server.vercel.app";
+export const backend_uri="https://binary-planet.onrender.com";
 
